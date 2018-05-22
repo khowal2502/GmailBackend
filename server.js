afc83b120e9119e782808d6d7a807e6a1a656dc9
Krishna Khowal <krishna.khowal@outlook.com>
2018-05-22 16:01:10 +0530

    Create call for Drafts

---
+++ server.js
@@ -0,0 +1,60 @@
+var express    = require('express');        
+var app        = express();                 
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+
+router.get('/login', function(req, res) {
+    res.json({ message: 'Login Successful!' });   
+});
+
+//First it will always be created as Drafts
+router.post('/create', function(req, res){
+    var owner_id = req.body.user_id;
+    var subject = req.body.subject;
+    var body = req.body.body;
+
+    var email_id = req.body.id;
+
+    var sql = "";
+    if(email_id===undefined){
+        sql = "INSERT INTO Emails(owner_id, subject, body, type) VALUES \
+        (" + owner_id + ", '" + subject + "', '" + body + "', 'draft')";
+    }else{
+        sql = "UPDATE Emails SET owner_id = " + owner_id + ", subject = '" + subject + "', body = '" + body + "' WHERE \
+                id = " + email_id;
+    }
+
+    console.log(sql);
+    
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Email saved as draft!");
+        res.json({"message": "Done!"});
+    });
+});
+
+
+app.use('/', router);
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "krishna",
+    password: "krishna1234",
+    database: "gmail",
+    insecureAuth: true
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to Database!");
+    app.listen(port);
+    console.log('Mail Server up on port ' + port);
+});
